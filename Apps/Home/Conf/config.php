7dbb8073b012146ffe1e3546c2324e3afaac9ff8
sushe <stupboy@163.com>
2015-11-11 01:24:15 +0800

    THINKPHP 学习到配置加载

---
+++ Apps/Home/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+'DEFAULT_MODULE' => 'Index',//默认模块
+'URL_MODEL'      => '2',//URL模式
+'SESSION_AUTO_START' => true,//是否开启session
+'USER_CONFIG' => array(
+ 'USER_AUTH' => true,
+ 'USER_TYPE' => 2,
+)
+	//'配置项'=>'配置值'
+);
--- index.php
+++ index.php
@@ -16,9 +16,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
-
+//define('CONF_EXT','.ini'); 配置文件格式 默认为PHP
 // 定义应用目录
-define('APP_PATH','./Application/');
+define('APP_PATH','./Apps/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
